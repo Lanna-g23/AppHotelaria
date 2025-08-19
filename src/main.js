a46a11bfbaa39df003e84113cc4deb2a16f6ede3
lanna giullia <lannagiullia45@gmail.com>
2025-08-19 16:24:06 -0300

    teste
    primeiro commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import renderLoginPage from "./pages/login.js";
+import renderRegisterPage from "./pages/registra.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderRegisterPage();
+});
---
+++ src/pages/login.js
@@ -0,0 +1,14 @@
+import Form from "../components/Form.js";
+import Navbar from "../components/Navbar.js";
+
+export default function renderLoginPage() { 
+    const nav = document.getElementById('navbar');
+    nav.innerHTML = '';
+    
+    const navbar = Navbar();
+    nav.appendChild(navbar);
+
+    Form();
+
+}
+
