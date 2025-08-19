6fd9c6b9a8fbcd128721de8c4e22d5e76e1968ae
lanna giullia <lannagiullia45@gmail.com>
2025-08-19 16:58:08 -0300

    atualização
    commt -m

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 import renderLoginPage from "./pages/login.js";
 import renderRegisterPage from "./pages/registra.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-    renderRegisterPage();
-});
+//configuração de rotas mapeadas
+const routes = {
+    "/login": renderLoginPage,
+    "/register": renderRegisterPage
+    //Novas páginas aqui adicionadas conforme desenvolvidas
+};
+
+//obtém o caminho atual a partir do hash de URL
+function getPath(){
+    //obtém o hash (ex. "#/login"), remove o # e tira espaços
+    const url = (location.hash || "").replace(/^#/, "").trim();
+    //retorna url se começar com "/", se não, retorna "/login" como padrão 
+    return url && url.startsWith("/") ? url : "/login";
+}
+    //Decide o que renderizar com base na rota atual
+    function renderRoutes(){
+        const url = getPath(); //lé a rota atual, ex. "/register"
+        const render = routes[url] || routes["/login"]; //Busca esta ruta no mapa
+        render(); //Executa a funçao de render na página atual 
+}
+
+window.addEventListener("hashchange", renderRoutes);
+//renderização
+document.addEventListener('DOMContentLoaded', renderRoutes);
+
