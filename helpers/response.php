e8cb3f3a89b0cd7ef749097b232e6e6c12bf1605
lanna giullia <lannagiullia45@gmail.com>
2025-09-25 13:57:43 -0300

    Update response.php

--- helpers/response.php
+++ helpers/response.php
@@ -1,17 +1,9 @@
 <?php
 <<<<<<< HEAD
-=======
-
->>>>>>> 27e06b41d1d2d52b11bd88228369c26bf1bc5d5b
 function jsonResponse($data, $status=200){
     http_response_code($status);
     header("Content-Type: application/json; charset=utf-8");
     echo json_encode($data, JSON_UNESCAPED_UNICODE);
     exit;
-<<<<<<< HEAD
-=======
-
->>>>>>> 27e06b41d1d2d52b11bd88228369c26bf1bc5d5b
-}
 
 ?>
