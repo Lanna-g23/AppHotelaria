687fea643f5db12cbf43d307656b12f9a8c1bb47
lanna giullia <lannagiullia45@gmail.com>
2025-09-25 14:06:33 -0300

    Update cliente.php

--- routes/cliente.php
+++ routes/cliente.php
@@ -7,39 +7,27 @@
         if(isset($id)){
             ClienteController::getBydId($conn,$id);
         }else{
-<<<<<<< HEAD
+
             ClienteController::listarTodos($conn);
-=======
-            ClienteController::getAll($conn);
->>>>>>> 27e06b41d1d2d52b11bd88228369c26bf1bc5d5b
         }
     }
     elseif($_SERVER['REQUEST_METHOD'] === "PUT"){
         $data = json_decode(file_get_contents("php://input"), true);
         $id = $data["id"] ?? null;
-<<<<<<< HEAD
+
         ClienteController::update($conn, $id, $data);
-=======
-        ClienteController::update($conn,$id, $data);
->>>>>>> 27e06b41d1d2d52b11bd88228369c26bf1bc5d5b
     }
 
     elseif($_SERVER['REQUEST_METHOD'] === "DELETE"){
         $data = json_decode(file_get_contents("php://input"), true);
         $id = $data["id"] ?? null;
-<<<<<<< HEAD
-        ClienteController::delete($conn, $id);
-=======
+
         ClienteController::delete($conn,$id);
->>>>>>> 27e06b41d1d2d52b11bd88228369c26bf1bc5d5b
     }
 
     elseif($_SERVER['REQUEST_METHOD'] === "POST"){
         $data = json_decode(file_get_contents("php://input"), true);
-<<<<<<< HEAD
-        ClienteController::create($conn, $data);
-=======
+
         ClienteController::create( $conn, $data);
->>>>>>> 27e06b41d1d2d52b11bd88228369c26bf1bc5d5b
     }
 ?>
