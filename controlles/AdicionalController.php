35a4a5caf849b3472f487f52d00c6a83f876788a
lanna giullia <lannagiullia45@gmail.com>
2025-09-29 13:58:37 -0300

    Atualização_12

--- controlles/AdicionalController.php
+++ controlles/AdicionalController.php
@@ -1,9 +1,11 @@
 <?php
 require_once  __DIR__ . "/../models/AdicionalModel.php";
+require_once  __DIR__ . "/../models/AdditionalModel.php";
 
 class AdicionalController{
         public static function create($conn, $data){
             $result = AdicionalModel::create($conn, $data);
+            $result = AdditionalModel::create($conn, $data);
             if($result){
                 return jsonResponse(['message'=> 'Adicionais criados com sucesso']);
             }else{
@@ -16,16 +18,19 @@ class AdicionalController{
 
         public static function getAll($conn){
             $listarAdicional = AdicionalModel::getAll($conn);
+            $listarAdicional = AdditionalModel::getAll($conn);
             return jsonResponse($listarAdicional);
         }
 
         public static function getById($conn, $id){
             $listarporId = AdicionalModel::getById($conn, $id);
+            $listarporId = AdditionalModel::getById($conn, $id);
             return jsonResponse($listarporId);
         }
 
         public static function delete($conn, $id){
             $result = AdicionalModel::delete($conn, $id);
+            $result = AdditionalModel::delete($conn, $id);
             if($result){
                 return jsonResponse(['message'=> 'Adicionais deletado com sucesso']);
             }else{
@@ -35,6 +40,7 @@ class AdicionalController{
 
         public static function update($conn, $id, $data){
             $result = AdicionalModel::update($conn, $id, $data);
+            $result = AdditionalModel::update($conn, $id, $data);
             if($result){
                 return jsonResponse(['message'=> 'Adicional atualizado com sucesso.']);
             }else {
